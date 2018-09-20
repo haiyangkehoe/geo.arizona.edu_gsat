a68f9c117ab1fc17d40f04aeeec8593d333f0bb8
haiyangkehoe <haiyang.kehoe@gmail.com>
2018-09-19 22:23:43 -0700

    Better side navbar

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-function openNav() {
+/* Toggle side navbar */
+function openCloseNav() {
+  var movebtn = document.getElementById("movebtn");
+  var hpos = movebtn.offsetLeft;
+  if (hpos == 0) {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("shift").style.marginLeft = "250px";
+    movebtn.style.marginLeft = "250px";
+    document.getElementById("arrow").textContent="<";
+  } else {
+    document.getElementById("mySidenav").style.width = "0";
+    movebtn.style.marginLeft = "0";
+    document.getElementById("arrow").textContent=">";
+  }
 }
 
+/* Close side navbar */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("shift").style.marginLeft = "0";
+    document.getElementById("movebtn").style.marginLeft = "0";
+    document.getElementById("arrow").textContent=">";
 }
 
 /* Sticky navbar anchor offset */
